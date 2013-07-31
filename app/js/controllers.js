84910b20622b4d7b8728b97066f66bca7b8b96dd
Christian Lambert <christianlambertwork@gmail.com>
2013-07-31 16:05:26 -0600

    Started work on layer drawing.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@
 'use strict';
 
 /* Controllers */
-
+var Layers;
 function InputCtrl($scope) {
 	$scope.numLayers = [1,2,3];
 	
@@ -32,6 +32,8 @@ function InputCtrl($scope) {
     if (this.numLayers.length < 30) {
 	    var layerNum = this.numLayers.length + 1;
 	    this.numLayers.push(layerNum);
+	    Layers = numLayers;
+	    return Layers;
   }
   	else {
   		alert('Sorry, no more than 30 layers.');
@@ -41,11 +43,17 @@ function InputCtrl($scope) {
   	if (this.numLayers.length > 1) {
 	  	var layerNum = this.numLayers.length;
 	  	this.numLayers.pop(layerNum);
+	  	Layers = numLayers;
+	  	return Layers;
 	  } else {
 	  	alert('You need at least 1 layer.')
 	  }
   }
   $scope.submit = function() {
-  	
+  	//send layer values and thicknesses to server for processing
+  	for (layer in this.numLayers) {
+  	//TODO: grab layer info and send it to server. Figure out how to do that.
+  		
+  	};
   }
 };
--- app/js/layers.js
+++ app/js/layers.js
@@ -1,9 +1,15 @@
 /**
  * @author Christian Lambert
-*/ 
+*/
+// Material colors 
+var aluminum = '#999999'
+var water = '#66CCFF';
+var lead = '#3D3D3D';
+var kevlar
 
-
-var layers = document.getElementById('layerCanvas');
-var l = layers.getContext("2d");
-l.fillStyle='blue';
-l.fillRect(230,75,20,20);
+var layerCanvas = document.getElementById("layerCanvas");
+var layers = layerCanvas.getContext("2d");
+layers.fillStyle=water;
+layers.fillRect(230,75,20,20);
+layers.fillStyle=aluminum;
+layers.fillRect(205,65,20,40);
