4c0bf8ba422e4ea95e58aa65b6799e1a335e930d
Christian Lambert <christianlambertwork@gmail.com>
2013-08-06 14:35:49 -0600

    Worked on sumbitting data and created a server folder

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,7 @@
 var Layers;
 function InputCtrl($scope) {
 	$scope.numLayers = [1,2,3];
-	
+	// This is an array of the possible materials
   $scope.materials = [
     {"name": " None",
     "value": "none"
@@ -26,8 +26,11 @@ function InputCtrl($scope) {
      }
     
   ];
-
+ 
   $scope.orderProp = 'name';
+  /* This function adds one layer if there are less than 30 layers
+   * if there are thirty layers it will tell the user that they can't add anymore.
+   */ 
   $scope.addLayer = function() {
     if (this.numLayers.length < 30) {
 	    var layerNum = this.numLayers.length + 1;
@@ -39,6 +42,10 @@ function InputCtrl($scope) {
   		alert('Sorry, no more than 30 layers.');
   	}
  } 	
+ /* This function will remove a layer until there is only
+  * one layer present. At that point it will alert the user telling
+  * them that they need at least on layer.
+  */
   $scope.removeLayer = function() {
   	if (this.numLayers.length > 1) {
 	  	var layerNum = this.numLayers.length;
@@ -49,15 +56,18 @@ function InputCtrl($scope) {
 	  	alert('You need at least 1 layer.');
 	  }
   }
+  //send layer values and thicknesses to server for processing
   $scope.submit = function() {
-  	//send layer values and thicknesses to server for processing
-  	var output = [];
-  	for (var layer in this.numLayers) {
+  	var layer;
+  	for (layer in this.numLayers) {
   	//TODO: grab layer info and send it to server. Figure out how to do that.
-  		var data = document.getElementById('layer' + layer);
-  		var material = document.getElementById('thick' + layer);
-  		output.push();
-  		console.log(output[layer]);
+		var layerId = 'layer' + (Math.round(layer) + 1);
+		var thickId = 'thick' + (Math.round(layer) + 1);
+		var unitId = 'unit' + (Math.round(layer) + 1);
+  		var material = document.getElementById(layerId).value;
+  		var thick = document.getElementById(thickId).value;
+  		var unit = document.getElementById(unitId).value;  	
+  		
   	};
   }
 };
